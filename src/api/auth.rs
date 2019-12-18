315fec376e2ecd5058d5e400a9b7972fd071f862
zhouyuxiang <1521350289@qq.com>
2019-12-18 22:38:14 +0800

    chore: signup api

---
+++ src/api/auth.rs
@@ -0,0 +1,8 @@
+// use crate::model::user::RegUser;
+use crate::DbAddr;
+use actix_web::{
+    error,
+    web::{Data, Json},
+};
+
+// pub fn signup(regist_user: Json<RegUser>, db: Data<DbAddr>) {}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate serde_derive;
+
 use actix::prelude::*;
-use actix_rt::System;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    web::{self, delete, get, post, put, resource, route, scope},
+    App, HttpResponse, HttpServer, Responder,
+};
 use diesel::mysql::MysqlConnection;
 use diesel::r2d2::{ConnectionManager, Pool};
 use listenfd::ListenFd;
+
+mod api;
 
 pub struct Dba(pub Pool<ConnectionManager<MysqlConnection>>);
 
@@ -31,13 +40,25 @@ fn index() -> impl Responder {
 
 // systemfd --no-pid -s http::3000 -- cargo watch -x run
 fn main() {
-    // let sys = System::new("actix_web_example");
+    let sys = actix::System::new("actix-web-example");
+    let addr: DbAddr = init_dba();
+    let bind_host = dotenv::var("BIND_ADDRESS").unwrap_or("127.0.0.1:3000".to_string());
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().route("/", web::get().to(index)));
+    let mut server = HttpServer::new(move || {
+        App::new()
+            .data(addr.clone())
+            .service(
+                scope("/api")
+                    // .service(resource("/signup").route(post().to_async(api::auth::signup))),
+            )
+            .route("/", web::get().to(index))
+    });
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
     } else {
-        server.bind("127.0.0.1:3000").unwrap()
+        server.bind(&bind_host).unwrap()
     };
-    server.run().unwrap();
+    server.start();
+    println!("Starting http server: {}", bind_host);
+    let _ = sys.run();
 }
