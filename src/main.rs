bbd10f00dc82d70c475d26f0de32f974d287964b
zhouyuxiang <1521350289@qq.com>
2019-12-20 23:52:16 +0800

    feat: static file server, log

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,20 @@ extern crate diesel;
 extern crate serde_derive;
 
 use actix::prelude::*;
+use actix_files as fs;
 use actix_web::{
+    dev,
+    http::{
+        self,
+        header::{ContentDisposition, DispositionType},
+    },
+    middleware::{
+        self,
+        errhandlers::{ErrorHandlerResponse, ErrorHandlers},
+        Logger,
+    },
     web::{self, delete, get, post, put, resource, route, scope},
-    App, HttpResponse, HttpServer, Responder,
+    App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
 use diesel::mysql::MysqlConnection;
 use diesel::r2d2::{ConnectionManager, Pool};
@@ -34,24 +45,44 @@ pub fn init_dba() -> DbAddr {
     SyncArbiter::start(cpu_num * 2 + 1, move || Dba(conn.clone()))
 }
 
-fn index() -> impl Responder {
+fn render_500<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
+    res.response_mut().headers_mut().insert(
+        http::header::CONTENT_TYPE,
+        http::HeaderValue::from_static("Error"),
+    );
+    Ok(ErrorHandlerResponse::Response(res))
+}
+
+fn index(req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let path: std::path::PathBuf = req.match_info().query("filename").parse().unwrap();
+    let file = fs::NamedFile::open(String::from("src/") + path.to_str().unwrap())?;
+    Ok(file.use_last_modified(true))
+}
+
+fn index1() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 // systemfd --no-pid -s http::3000 -- cargo watch -x run
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
     let sys = actix::System::new("actix-web-example");
     let addr: DbAddr = init_dba();
     let bind_host = dotenv::var("BIND_ADDRESS").unwrap_or("127.0.0.1:3000".to_string());
     let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new(move || {
         App::new()
+            .wrap(ErrorHandlers::new().handler(http::StatusCode::INTERNAL_SERVER_ERROR, render_500))
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(middleware::DefaultHeaders::new().header("X-Version", "0.2"))
             .data(addr.clone())
+            .route("/{filename:.*}", web::get().to(index))
             .service(
-                scope("/api")
-                    // .service(resource("/signup").route(post().to_async(api::auth::signup))),
+                scope("/api"), // .service(resource("/signup").route(post().to_async(api::auth::signup))),
             )
-            .route("/", web::get().to(index))
+            .route("/", web::get().to(index1))
     });
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
